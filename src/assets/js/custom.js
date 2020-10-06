b7f21d41efc1676f6d6b1cfe2fb34ca2cf539054
Tran Thi Thi <thittth1807067@fpt.edu.vn>
2020-10-06 17:01:45 +0700

    bỏ

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -7,18 +7,44 @@ $('#range_slider').ionRangeSlider({
     from: 10000,
     to: 500000
 });
+$.fn.select2.amd.require([
+  "select2/core",
+  "select2/utils"
+], function (Select2, Utils, oldMatcher) {
+  var $basicSingle = $(".js-example-basic-single");
+  var $basicMultiple = $(".js-example-basic-multiple");
 
+  $.fn.select2.defaults.set("width", "100%");
+
+  $basicSingle.select2();
+  $basicMultiple.select2();
+
+  function formatState (state) {
+    if (!state.id) {
+      return state.text;
+    }
+    var $state = $(
+      '<span>' +
+      '<img src="vendor/images/flags/' +
+      state.element.value.toLowerCase() +
+      '.png" class="img-flag" /> ' +
+      state.text +
+      '</span>'
+    );
+    return $state;
+  };
+});
 
 var countEl = document.getElementById("quantity");
     function plus(){
        if(countEl.value < 10) {
           countEl.value = parseInt(countEl.value) +1;
-       }  
-}  
-function minus(){  
+       }
+}
+function minus(){
 
-  if (countEl.value > 1) {  
+  if (countEl.value > 1) {
 
-    countEl.value -= 1;  
-  }    
+    countEl.value -= 1;
+  }
 }
