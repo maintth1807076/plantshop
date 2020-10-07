dd20a79c391733721cb54154ef650f9e27a1f6c5
Tran Thi Thi <thittth1807067@fpt.edu.vn>
2020-10-08 05:52:38 +0700

    chỉnh sửa lại giao diện

--- src/app/outsite/outsite.component.ts
+++ src/app/outsite/outsite.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./outsite.component.css']
 })
 export class OutsiteComponent implements OnInit {
-
+  total: number;
+  items: any[];
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCart();
   }
 
   async ngAfterViewInit() {
@@ -17,8 +19,6 @@ export class OutsiteComponent implements OnInit {
     await this.loadScript('/assets/js/plugins.js');
     await this.loadScript('/assets/js/functions.js');
     await this.loadScript('/assets/plugins/range-slider/rangeslider.js');
-    await this.loadScript('/assets/plugins/gmap3/gmap3.min.js');
-    await this.loadScript('/assets/plugins/gmap3/map-styles.js');
     await this.loadScript('/assets/js/custom.js');
 
   }
@@ -30,5 +30,34 @@ export class OutsiteComponent implements OnInit {
       document.body.appendChild(scriptElement);
     })
   }
+  loadCart(): void {
+    this.total = 0;
+    this.items = [];
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    for (var i = 0; i < cart.length; i++) {
+      let item = JSON.parse(cart[i]);
+      this.items.push({
+        "image": item.image,
+        "product" : item.product,
+        "quantity": item.quantity
+      });
+      this.total += item.product.price * item.quantity;
+    }
+    console.log(this.items)
+  }
 
+  remove(id: string): void {
+    let cart: any = JSON.parse(localStorage.getItem('cart'));
+    let index: number = -1;
+    for (var i = 0; i < cart.length; i++) {
+      let item = JSON.parse(cart[i]);
+      if (item.product.id == id) {
+        cart.splice(i, 1);
+        break;
+      }
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.loadCart();
+    console.log(this.items)
+  }
 }
