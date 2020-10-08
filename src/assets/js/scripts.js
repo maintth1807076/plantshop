5ef05f6252e8fcd421cfe21bc734595d46b24165
Tran Thi Thi <thittth1807067@fpt.edu.vn>
2020-10-09 01:59:36 +0700

    lấy nhiều danh mục

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -20,3 +20,30 @@
         $("body").toggleClass("sb-sidenav-toggled");
     });
 })(jQuery);
+$.fn.select2.amd.require([
+  "select2/core",
+  "select2/utils"
+], function (Select2, Utils, oldMatcher) {
+  var $basicSingle = $(".js-example-basic-single");
+  var $basicMultiple = $(".js-example-basic-multiple");
+
+  $.fn.select2.defaults.set("width", "100%");
+
+  $basicSingle.select2();
+  $basicMultiple.select2();
+
+  function formatState (state) {
+    if (!state.id) {
+      return state.text;
+    }
+    var $state = $(
+      '<span>' +
+      '<img src="vendor/images/flags/' +
+      state.element.value.toLowerCase() +
+      '.png" class="img-flag" /> ' +
+      state.text +
+      '</span>'
+    );
+    return $state;
+  };
+});
