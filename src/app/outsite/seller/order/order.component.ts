43624f4fd596891a2e4a585557ddb283c2b0df88
Tran Thi Thi <thittth1807067@fpt.edu.vn>
2020-10-20 20:33:24 +0700

    order

--- src/app/outsite/seller/order/order.component.ts
+++ src/app/outsite/seller/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {TreeService} from '../../../tree.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  userId:any;
+  id: any;
+  p: any;
+  listOrder: any[];
+  alive: boolean = true;
+
+  constructor(private http: HttpClient, private service: TreeService, private router: Router) {
+
+  }
 
   ngOnInit(): void {
+    let user = JSON.parse(localStorage.getItem('user'));
+    this.id = user['id'];
+    this.loadData();
+  }
+  async ngAfterViewInit() {
+    await this.loadScript('/assets/js/jquery.js');
+    await this.loadScript('/assets/js/datatables.min.js');
+    await this.loadScript('/assets/js/style.js');
+
+  }
+
+  loadScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    });
+  }
+  loadData(): void {
+    this.service.getOrderBySellerId(this.id).subscribe(data => {
+        this.listOrder = data['datas'];
+        console.log(data)
+      },
+      (error) => console.log(error),
+      () => console.log('Complete')
+    );
   }
 
 }
